f032d1c67fe9cce15f6662c0ec6df8707d81bb07
Geert Eltink <xtreamwayz@gmail.com>
2018-01-23 09:06:40 +0100

    Updated to final PSR-15

--- test/AuthenticationMiddlewareTest.php
+++ test/AuthenticationMiddlewareTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authentication for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authentication/blob/master/LICENSE.md New BSD License
  */
 
@@ -9,8 +9,8 @@ declare(strict_types=1);
 
 namespace ZendTest\Expressive\Authentication;
 
-use Interop\Http\Server\MiddlewareInterface;
-use Interop\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;

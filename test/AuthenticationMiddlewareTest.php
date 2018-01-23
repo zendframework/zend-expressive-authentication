d0fd422188158f71b8ba252e7dfaa56338d8a43f
Geert Eltink <xtreamwayz@gmail.com>
2018-01-23 10:13:47 +0100

    Fix issues per @webimpress

--- test/AuthenticationMiddlewareTest.php
+++ test/AuthenticationMiddlewareTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authentication for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authentication/blob/master/LICENSE.md New BSD License
  */
 
@@ -9,11 +9,11 @@ declare(strict_types=1);
 
 namespace ZendTest\Expressive\Authentication;
 
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Expressive\Authentication\AuthenticationInterface;
 use Zend\Expressive\Authentication\AuthenticationMiddleware;
 use Zend\Expressive\Authentication\UserInterface;

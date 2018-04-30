e7e466b85907e351468319d9956853f502a184a7
Geert Eltink <xtreamwayz@gmail.com>
2018-04-30 09:06:58 +0200

    Fix issues per @webimpress

--- src/UserRepository/SessionUser.php
+++ src/UserRepository/SessionUser.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authentication for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authentication/blob/master/LICENSE.md New BSD License
  */
 
@@ -13,15 +13,17 @@ use Zend\Expressive\Authentication\UserInterface;
 
 class SessionUser implements UserInterface
 {
+    /** @var string */
     private $identity;
 
+    /** @var array */
     private $roles;
 
     public static function fromState(array $data) : UserInterface
     {
         $user = new self();
 
-        $user->identity = $data['identity'];
+        $user->identity = (string) $data['identity'];
         $user->roles    = (array) $data['roles'];
 
         return $user;

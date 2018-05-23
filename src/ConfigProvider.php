e61771b661f7dbc5f46251165da36ccd514a0477
Matthew Weier O'Phinney <matthew@zend.com>
2018-05-23 11:17:48 -0500

    Adds trailing commas

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-authentication for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-authentication/blob/master/LICENSE.md New BSD License
  */
 
@@ -42,7 +42,7 @@ class ConfigProvider
              *             'password' => 'password field name',
              *         ],
              *         'sql_get_roles'   => 'SQL to retrieve user roles by :identity',
-             *         'sql_get_details' => 'SQL to retrieve user details by :identity'
+             *         'sql_get_details' => 'SQL to retrieve user details by :identity',
              *     ],
              * ]
              */
@@ -65,8 +65,8 @@ class ConfigProvider
                 AuthenticationMiddleware::class => AuthenticationMiddlewareFactory::class,
                 UserRepository\Htpasswd::class => UserRepository\HtpasswdFactory::class,
                 UserRepository\PdoDatabase::class => UserRepository\PdoDatabaseFactory::class,
-                UserInterface::class => DefaultUserFactory::class
-            ]
+                UserInterface::class => DefaultUserFactory::class,
+            ],
         ];
     }
 }
